405af0c0fa0735b7cfb1761970f53bf1a008cb3a
LinnierGames <esericksanc@gmail.com>
2018-02-10 13:23:44 -0800

    UNIUser.init(_ snapshot: DataSnapshot)

--- unicornchats/Model/User.swift
+++ unicornchats/Model/User.swift
@@ -7,10 +7,24 @@
 //
 
 import UIKit
+import Firebase
 
 class UNIUser: NSObject {
     
     var name: String?
     var email: String?
+    
+    init?(_ snapshot: DataSnapshot) {
+        guard
+            let dictionary = snapshot.value as! [String: AnyObject]?,
+            let nameValue = dictionary["name"] as! String?,
+            let emailValue = dictionary["email"] as! String?
+            else {
+                return nil
+        }
+        
+        self.name = nameValue
+        self.email = emailValue
+    }
 
 }
